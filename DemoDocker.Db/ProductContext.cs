55c72471215f668849982508a60305f88817f9fa
Rupesh <rupesh.kumar@tamtam.nl>
2017-07-08 08:56:47 +0200

    initial commit
    initial commit

---
+++ DemoDocker.Db/ProductContext.cs
@@ -0,0 +1,25 @@
+﻿using DemoDocker.Db.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDocker.Db
+{
+    public class ProductContext:DbContext
+    {
+        public ProductContext() : base() { }
+        public ProductContext(string connString)
+        : base(connString)
+    {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Product>().HasKey(p => p.Id);
+        }
+    }
+}
