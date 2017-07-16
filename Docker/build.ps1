3b0cb5c36e0b08605527df26f197f7f42899764b
Rupesh <rupesh.kumar@tamtam.nl>
2017-07-16 10:22:49 +0200

    updated dockerfile

--- Docker/build.ps1
+++ Docker/build.ps1
@@ -1,4 +1,4 @@
-$msBuildPath = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+$msBuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
 
 # publish website:
 & $msBuildPath ..\src\DemoDocker\DemoDocker.csproj /p:OutputPath=..\..\Docker\publish  /p:DeployOnBuild=true
---
+++ Docker/publish/_PublishedWebsites/DemoDocker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoDocker.MvcApplication" Language="C#" %>
